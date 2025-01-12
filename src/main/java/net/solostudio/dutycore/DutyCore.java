9a2d179004a8cd2e4fe9934e9720eb56fb0f5427
User-19fff <lolkd005@gmail.com>
2025-01-13 00:03:32 +0100

    Basics

---
+++ src/main/java/net/solostudio/dutycore/DutyCore.java
@@ -0,0 +1,17 @@
+package net.solostudio.dutycore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DutyCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
